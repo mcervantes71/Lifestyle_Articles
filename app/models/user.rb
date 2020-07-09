d8d10b43a96e2c29d1b510f2fbf0420471766a87
Martin Cervantes <cervantes.martine@gmail.com>
2020-07-09 15:57:10 -0600

    Add devise initial configuration

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable
+
+  validates :name, presence: true
+  validates :username, length: { in: 4..15 }, presence: true, uniqueness: true
+  validates :password, length: { in: 6..128 }, presence: true
+  validates :password, confirmation: { case_sensitive: true }
+end
