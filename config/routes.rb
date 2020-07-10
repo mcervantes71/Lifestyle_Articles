ee18d1bddd715c8214174087b30eeaca5f37482e
Martin Cervantes <cervantes.martine@gmail.com>
2020-07-09 19:59:40 -0600

    Add articles views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  root to: 'articles#index'
 
   devise_for :users
+
+  resources :users, only: [:index, :show]
+  resources :articles
 end
