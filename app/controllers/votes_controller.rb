f9fcc117999a57f203ba4c403b27785a708aa98d
Martin Cervantes <cervantes.martine@gmail.com>
2020-07-14 11:10:35 -0600

    Add number of votes to articles

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,14 @@
 class VotesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @vote = current_user.votes.build(article_id: params[:article_id])
 
     if @vote.save
-      redirect_to root_path, notice: 'You voted an article.'
+      a = Article.find(params[:article_id])
+      c = a.categories
+
+      redirect_to article_path(c[0].category_name), notice: 'You voted an article.'
     else
       redirect_to root_path, alert: 'You cannot vote this article.'
     end
@@ -13,9 +16,10 @@ class VotesController < ApplicationController
 
   def destroy
     vote = Vote.find_by(user: current_user, article_id: params[:article_id])
+
     if vote
       vote.destroy
-      redirect_to root_path, notice: 'You unvote the article.'
+      redirect_to article_path, notice: 'You unvote the article.'
     else
       redirect_to root_path, alert: 'You cannot unvote the article that you did not vote before.'
     end
